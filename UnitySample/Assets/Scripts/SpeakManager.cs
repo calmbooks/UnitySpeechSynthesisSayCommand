b86bd9c1088079f0e8620d6b7a016194ca0f6c41
Yamato Honda <honda-yamato@kayac.com>
2014-07-22 09:51:08 +0900

    create code

---
+++ UnitySample/Assets/Scripts/SpeakManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Diagnostics;
+
+public class SpeakManager {
+
+    #region singleton
+    private static SpeakManager mInstance;
+
+    public static SpeakManager Instance {
+
+        get {
+
+            if( mInstance == null ) {
+
+                mInstance = new SpeakManager();
+            }
+
+            return mInstance;
+        }
+    }
+    #endregion
+
+    public delegate void SayEndDelegate();
+
+    public event SayEndDelegate SayEndHandlers;    
+
+    public void Say ( string msg ) {
+
+        Process process = new Process();
+
+        process.StartInfo.FileName="say";
+        process.StartInfo.Arguments = msg;
+
+        process.EnableRaisingEvents = true;
+        process.Exited += SayEnd;
+
+        process.Start();
+    }
+
+    void SayEnd ( object sender, System.EventArgs e ) {
+
+        SayEndHandlers();
+    }
+}
---
+++ UnitySample/Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    SpeakManager speakManager;
+
+	void Start () {
+
+        speakManager = new SpeakManager();	
+
+        speakManager.Say("こんにちは 世界");
+
+        speakManager.SayEndHandlers += SayEnd;
+	}
+
+    void SayEnd () {
+
+        Debug.Log("SayEnd");
+    }
+}
